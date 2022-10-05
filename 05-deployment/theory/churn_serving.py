d70f0c486d759bc3c3f30840dd755b8d95fd4bf1
lipinor <filipe.cbpf@gmail.com>
2022-10-05 16:05:56 -0300

    removed comments from churn_serving

--- 05-deployment/theory/churn_serving.py
+++ 05-deployment/theory/churn_serving.py
@@ -10,38 +10,6 @@ def predict_single(customer, dv, model):
 with open('churn-model.bin', 'rb') as f_in:
     dv, model = pickle.load((f_in))
 
-# customer = {
-#             'customerid': '8879-zkjof',
-#             'gender': 'female',
-#             'seniorcitizen': 0,
-#             'partner': 'no',
-#             'dependents': 'no',
-#             'tenure': 41,
-#             'phoneservice': 'yes',
-#             'multiplelines': 'no',
-#             'internetservice': 'dsl',
-#             'onlinesecurity': 'yes',
-#             'onlinebackup': 'no',
-#             'deviceprotection': 'yes',
-#             'techsupport': 'yes',
-#             'streamingtv': 'yes',
-#             'streamingmovies': 'yes',
-#             'contract': 'one_year',
-#             'paperlessbilling': 'yes',
-#             'paymentmethod': 'bank_transfer_(automatic)',
-#             'monthlycharges': 79.85,
-#             'totalcharges': 3320.75,
-#             }
-
-# prediction = predict_single(customer, dv, model)
-
-# print('Prediction: %.3f' % prediction)
-
-# if prediction >= 0.5:
-#     print('Verdict: Churn')
-# else:
-#     print('Verdict: Not churn')
-
 app = Flask('churn')
 
 @app.route('/predict', methods=['POST']) #A
